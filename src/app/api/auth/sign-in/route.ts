0963af2b01b939816b28a39c35398367b140bdee
Yohannesdejene <yohannesdejene23@gmail.com>
2025-07-15 11:40:16 +0300

    build error fixed

--- src/app/api/auth/sign-in/route.ts
+++
@@ -1,4 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { db } from "@/db";
-
-import { eq } from "drizzle-orm";
--- src/app/api/auth/sign-up/route.ts
+++
@@ -1,4 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { db } from "@/db";
-import { user } from "@/db/schema";
-import { eq } from "drizzle-orm";
