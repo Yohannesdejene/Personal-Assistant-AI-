0a6460cde6eec21bb2b892f24da3c63f5db58777
Yohannesdejene <yohannesdejene23@gmail.com>
2025-07-28 12:55:36 +0300

    better auth url changed

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", // The base URL of your auth server
+  baseURL: "https://aiassit.vercel.app/", // The base URL of your auth server
 });
 
 
